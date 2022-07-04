bbf22a9cad1001eb59ba8ef0d5bf80a6625a4966
John-Luke Ward <74541834+Picardward94@users.noreply.github.com>
2022-07-04 16:29:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// let parent = document.getElementById("parent");
+// let reDiv = document.getElementById("resize");
+
+
+// $(document).ready(function () {
+//     $("#resize").resizable() 
+// });
+
+// let resWid = document.getElementById("resize").offsetWidth;
+
+// parent.addEventListener("click", resize);
+
+// function resize() {
+
+//     console.log(resWid);
+// }
